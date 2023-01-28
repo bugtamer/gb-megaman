c8f7ace9af214989148fa6b6fe2d2474d945bd21
bugtamer <35902230+bugtamer@users.noreply.github.com>
2023-01-28 18:02:10 +0100

    new AllPasswordsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllPasswordsComponent } from './components/all-passwords/all-passwords.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPasswordsComponent
   ],
   imports: [
     BrowserModule
